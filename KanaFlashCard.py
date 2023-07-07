5eb4d6280ba48ecd12a24de8d09e16281cc70b1b
Yurii287 <124893639+Yurii287@users.noreply.github.com>
2023-07-07 15:41:45 +0100

    Add files via upload

--- KanaFlashCard.py
+++ KanaFlashCard.py
@@ -1,6 +1,6 @@
 import random
-import tkinter as tk
-from tkinter import ttk
+import customtkinter
+from tkinter import *
 from threading import Thread
 
 # Entry Box & Buttons for submitting answers
@@ -8,55 +8,59 @@ from threading import Thread
 #   - Randomly select a key:value to print for the label
 # Have menu bar for quit options
 # Show progress bar of how far through the session you are
-# Use a 2 dictionaries for hiragana and katakana to relate to the sounds
 # In future add the dakten
+#
+# Use CustomTkinter
 
-katakana = {"ア":"a",
-            "イ":"i",
-            "ウ":"u",
-            "エ":"e",
-            "オ":"o", 
-            "カ":"ka",
-            "キ":"ki",
-            "ク":"ku",
-            "ケ":"ke",
-            "コ":"ko",
-            "サ":"sa",
-            "シ":"shi",
-            "ス":"su",
-            "セ":"se",
-            "ソ":"so",
-            "タ":"ta",
-            "チ":"chi",
-            "ツ":"tsu",
-            "テ":"te",
-            "ト":"to",
-            "ナ":"na",
-            "ニ":"ni",
-            "ヌ":"nu",
-            "ネ":"ne",
-            "ノ":"no",
-            "ハ":"ha",
-            "ヒ":"hi",
-            "フ":"fu",
-            "ヘ":"he",
-            "ホ":"ho",
-            "マ":"ma",
-            "ミ":"mi",
-            "ム":"mu",
-            "メ":"me",
-            "モ":"mo",
-            "ヤ":"ya",
-            "ユ":"yu",
-            "ヨ":"yo",
-            "ラ":"ra",
-            "リ":"ri",
-            "ル":"ru",
-            "レ":"re",
-            "ロ":"ro",
-            "ワ":"wa",
-            "ヲ":"wo",
-            "ン":"n"}
+class Katakana:
+    def __init__(self):
+        self.katakana = {"ア":"a",
+                        "イ":"i",
+                        "ウ":"u",
+                        "エ":"e",
+                        "オ":"o", 
+                        "カ":"ka",
+                        "キ":"ki",
+                        "ク":"ku",
+                        "ケ":"ke",
+                        "コ":"ko",
+                        "サ":"sa",
+                        "シ":"shi",
+                        "ス":"su",
+                        "セ":"se",
+                        "ソ":"so",
+                        "タ":"ta",
+                        "チ":"chi",
+                        "ツ":"tsu",
+                        "テ":"te",
+                        "ト":"to",
+                        "ナ":"na",
+                        "ニ":"ni",
+                        "ヌ":"nu",
+                        "ネ":"ne",
+                        "ノ":"no",
+                        "ハ":"ha",
+                        "ヒ":"hi",
+                        "フ":"fu",
+                        "ヘ":"he",
+                        "ホ":"ho",
+                        "マ":"ma",
+                        "ミ":"mi",
+                        "ム":"mu",
+                        "メ":"me",
+                        "モ":"mo",
+                        "ヤ":"ya",
+                        "ユ":"yu",
+                        "ヨ":"yo",
+                        "ラ":"ra",
+                        "リ":"ri",
+                        "ル":"ru",
+                        "レ":"re",
+                        "ロ":"ro",
+                        "ワ":"wa",
+                        "ヲ":"wo",
+                        "ン":"n"}
+    randKatakana = "ア"
 
 class Hiragana:
     def __init__(self):
@@ -107,36 +111,54 @@ class Hiragana:
                         "を":"wo",
                         "ん":"n"}
 
-    def hiragana_indx(self):
-        pass
-    
-    def submit(self):
-        print("OOP working")
+    rand_H = "あ"
 
-class App(tk.Tk):
+class MyFrame(customtkinter.CTkFrame,Hiragana):
+    
+    result = "Incorrect"
+    result_color= "red"
+    
+    def __init__(self, master,):
+        super().__init__(master)
+        
+        self.label = customtkinter.CTkLabel(self,text=Hiragana.rand_H,font=("Calibri",80))
+        self.label.grid(row=0,column=0,pady=25)
+        
+        self.entry = customtkinter.CTkEntry(self,font=("Calibri",28),width=250)
+        self.entry.grid(row=1,column=0,pady=25)
+        
+        self.submit = customtkinter.CTkButton(self,font=("Calibri",28),command=lambda: MyFrame.submit(),text="Submit")
+        self.submit.grid(row=2,column=0,pady=25)
+        
+        self.new_card = customtkinter.CTkButton(self,font=("Calibri",28),command=lambda: MyFrame.index(self.label),text="New Card")
+        self.new_card.grid(row=3,column=0,pady=25)
+        
+        self.result = customtkinter.CTkLabel(self,font=("Calibri",28),text=MyFrame.result,text_color=MyFrame.result_color)
+        self.result.grid(row=4,column=0)
+    
+    def index(label):
+        print(Hiragana.rand_H)
+        label.configure(text="New Text")
+        
+        
+    def submit():
+        print("Answer Submitted")
+
+
+class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         self.title("Kana Flashcards")
         self.geometry("640x480")
-        #all tkinter configs go here
         
-        #Weight the window - scalable centred elements
-        self.grid_rowconfigure(0,weight=0)
-        self.grid_columnconfigure(0,weight=1)
+        customtkinter.set_appearance_mode("System")
+        customtkinter.set_default_color_theme("green")
         
-        #label
-        self.flashcard = tk.Label(self,text="ロ",font=("",40))
-        self.flashcard.grid(row=0,column=0)
-        
-        #entry
-        self.answer_box = tk.Entry(self,font=("Arial",24))
-        self.answer_box.grid(row=1,column=0)
-
-        #submit button
-        self.submit = tk.Button(self,text="Submit",command=lambda: Hiragana.submit(self),font=("Arial",18))
-        self.submit.grid(row=2,column=0)
+        self.my_frame = MyFrame(master=self)
+        self.my_frame.pack()
 
 
 if __name__ == "__main__":
     app = App()
     app.mainloop()
+    
