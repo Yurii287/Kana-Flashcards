9f80735e414be971c65fed77f4b880aceeae20f2
Yurii287 <124893639+Yurii287@users.noreply.github.com>
2023-08-03 14:12:55 +0100

    Add files via upload

--- KanaFlashCardOOP.py
+++ KanaFlashCardOOP.py
@@ -1,9 +1,7 @@
 import random
-import customtkinter
-from CTkMen
-#import tkinter as tk
-#from tkinter import *
-#from tkinter import ttk
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
 katakana_dict = {"ア":"a",
                 "イ":"i",
@@ -151,7 +149,7 @@ katakana_dakuten_dict= {"ガ":"ga",
                         "ペ":"pe",
                         "ポ":"po"}
 
-class App(customtkinter.CTk):
+class App(tk.Tk):
     def __init__(self,title,size):
         super().__init__()
 #Variables
@@ -160,11 +158,11 @@ class App(customtkinter.CTk):
         H_dakuten = False
         K_dakuten = False
 #String Var
-        Hiraganastring_variable = customtkinter.StringVar(self,"Start")
+        Hiraganastring_variable = tk.StringVar(self,"Start")
         
-        Katakanastring_variable = customtkinter.StringVar(self,"Start")
+        Katakanastring_variable = tk.StringVar(self,"Start")
         
-        Resultstring_variable = customtkinter.StringVar(self," ")
+        Resultstring_variable = tk.StringVar(self," ")
 #Window Config
         self.title(title)
         self.geometry(f'{size[0]}x{size[1]}')
@@ -174,10 +172,10 @@ class App(customtkinter.CTk):
         
         self.katakanaframe = KatakanaFrame(self,Resultstring_variable,Katakanastring_variable,answer,score)
 #Main Screen
-        self.wlcmLabel = customtkinter.CtkLabel(self,text="Welcome",font=("Calibri",32))
+        self.wlcmLabel = tk.Label(self,text="Welcome",font=("Calibri",32))
         self.wlcmLabel.pack()
         
-        self.wlcm2Label = customtkinter.CtkLabel(self,text="Activate dakuten within the navbar",font=("Calibri",32))
+        self.wlcm2Label = tk.Label(self,text="Activate dakuten within the navbar",font=("Calibri",32))
         self.wlcm2Label.pack()  
 #NavBar
         menubar = Menu(self)
@@ -262,7 +260,7 @@ class HiraganaFrame(ttk.Frame):
                                 answer.pop(0)
 
 
-        self.Mainlabel = customtkinter.CtkLabel(self,textvariable=Hiraganastring_variable,font=("Calibri",64))
+        self.Mainlabel = tk.Label(self,textvariable=Hiraganastring_variable,font=("Calibri",64))
         self.Mainlabel.pack()
         
         self.startbtn = tk.Button(self,text="Start",command=lambda: Start(self),font=("Calibri",24))
@@ -271,10 +269,10 @@ class HiraganaFrame(ttk.Frame):
         self.Userentry = tk.Entry(self,font=("Calibri",24))
         self.Userentry.pack()
         
-        self.resultLabel = customtkinter.CtkLabel(self,textvariable=Resultstring_variable,font=("Calibri",32))
+        self.resultLabel = tk.Label(self,textvariable=Resultstring_variable,font=("Calibri",32))
         self.resultLabel.pack()
         
-        self.scoreLabel = customtkinter.CtkLabel(self,text=str(self.score),font=("Calibri",24))
+        self.scoreLabel = tk.Label(self,text=str(self.score),font=("Calibri",24))
         self.scoreLabel.pack()
         
         
